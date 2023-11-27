306e2e2dadbf768d0964b269e6c493e5dad74462
tanxiao <tanxiao@deepvision-tech.com>
2023-11-27 17:36:02 -0500

    finish set_device

--- hc_hack/main_sh.py
+++ hc_hack/main_sh.py
@@ -1,4 +1,6 @@
 from enum import Enum, auto
+from typing import Optional
+from .device import DeviceDB, ChipInfo
 
 
 class RunType(Enum):
@@ -18,7 +20,8 @@ class HCTaskEngine:
     """
 
     def __init__(self):
-        pass
+        self.device_db = DeviceDB()
+        self.chip_info: Optional[ChipInfo] = None
 
     def add_file(self, *files, ftype=None) -> "HCTaskEngine":
         pass
@@ -38,6 +41,26 @@ class HCTaskEngine:
         """
         if device_version == "NA":
             device_version = ""
+        if name is None or device_version is not None:
+            parts = self.device_db.find_by_part_number(part_number)
+            for chip_info in parts:
+                if chip_info.device_version == device_version:
+                    self.chip_info = chip_info
+                    break
+            else:
+                if len(parts) == 1:
+                    self.chip_info = parts[0]
+                else:
+                    raise ValueError(f"multiple devices found with part number {part_number}, specify version or name")
+        elif name is not None:
+            self.chip_info = self.device_db.find(part_number, name)
+        if self.chip_info is None:
+            raise ValueError(f"no such a device: {part_number} with name {name}, device_version {device_version}")
+        if name is not None and name != self.chip_info.name:
+            raise ValueError(f"no such a device: {part_number} with name {name}, device_version {device_version}")
+        if device_version is not None and device_version != self.chip_info.device_version:
+            raise ValueError(f"no such a device: {part_number} with name {name}, device_version {device_version}")
+        return self
 
     def run(self, rtype: RunType) -> "HCTaskEngine":
         pass
